3a24e8e4744c2ed9ee47af7d119e030610a976b0
marianoberton <marianoberton@gmail.com>
2017-10-23 12:03:58 -0300

    23 oct

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,56 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', function () {
+    return 'Hola!';
+});
+
+Route::get('/miprimerruta/registro', function () {
+    return 'primer ruta';
+});
+
+Route::get('/resultado/{numero}', function ($numero) {
+    if ($numero %2 ==0){
+      $resultado = "la pagina es par";
+    }else {
+      $resultado = "la pagina es impar";
+    };
+    return $resultado;
+});
+
+Route::get('/suma/{numero}/{numero2}', function ($numero,$numero2) {
+    return $numero + $numero2;
+});
+
+
+Route::get('/ejercicio3/{numero}/{numero2?}', function ($numero,$numero2=null) {
+    if ($numero2 ==null){
+        if ($numero %2 ==0){
+          $resultado = "la pagina es par";
+        }else {
+          $resultado = "la pagina es impar";
+        };
+    }else {
+        $resultado = $numero * $numero2;
+          };
+    return $resultado;
+});
+
+
+Route::get('/ejercicio4/{nombre?}', function ($nombre=null) {
+
+    if ($nombre !==null){
+      return 'Hola '.$nombre;
+    };
+    return 'Hola persona desconocida';
+});
+
+Route::get('/potencia/{numero}/{numero2?}', function ($numero,$numero2=null) {
+    if ($numero2 ==null){
+        $resultado = $numero * $numero;
+    }else {
+        $resultado = $numero ** $numero2;
+          };
+    return $resultado;
+});
